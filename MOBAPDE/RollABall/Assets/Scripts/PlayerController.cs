0a115c6c4db96490b83b725e64d5e6b328cb507e
Noodle <arcaramon@gmail.com>
2016-03-15 19:56:47 +0800

    multi-touch input

--- MOBAPDE/RollABall/Assets/Scripts/PlayerController.cs
+++ MOBAPDE/RollABall/Assets/Scripts/PlayerController.cs
@@ -23,26 +23,29 @@ public class PlayerController : MonoBehaviour {
         float moveHorizontal = 0.0f;
         float moveVertical = 0.0f;
 		if (Application.platform == RuntimePlatform.Android) {
-			var touch = Input.GetTouch (0);
-			Vector3 touchPos = new Vector3(0.0f, 0.0f, 0.0f);
+            var touchCount = Input.touchCount;
 
-            touchPos = touch.position;
-			int maxX = Screen.width - 1;
-			int maxY = Screen.height - 1;
+            for (var t = 0; t < touchCount; t++){
 
-			if (touchPos.x <= maxX && touchPos.x > maxX - 100 && touchPos.y < maxY - 100 && touchPos.y > 100) {
-				moveHorizontal = 1.0f;
-			} 
-			else if (touchPos.x >= 0 && touchPos.x <= 100 && touchPos.y < maxY - 100 && touchPos.y > 100) {
-				moveHorizontal = -1.0f;
-			} 
-			if (touchPos.y <= maxY && touchPos.y > maxY - 100 && touchPos.x < maxX - 100 && touchPos.x > 100) {
-                moveVertical = 1.0f;
-			}
-            else if(touchPos.y >= 0 && touchPos.y <= 100 && touchPos.x < maxX - 100 && touchPos.x > 100) {
-                moveVertical = -1.0f;
+                var touch = Input.GetTouch(t);
+                Vector3 touchPos = new Vector3(0.0f, 0.0f, 0.0f);
+                touchPos = touch.position;
+                int maxX = Screen.width - 1;
+                int maxY = Screen.height - 1;
+
+                if (touchPos.x <= maxX && touchPos.x > maxX - 100 && touchPos.y < maxY - 100 && touchPos.y > 100)
+                    moveHorizontal = 1.0f;
+                
+                else if (touchPos.x >= 0 && touchPos.x <= 100 && touchPos.y < maxY - 100 && touchPos.y > 100)
+                    moveHorizontal = -1.0f;
+                
+                if (touchPos.y <= maxY && touchPos.y > maxY - 100 && touchPos.x < maxX - 100 && touchPos.x > 100)
+                    moveVertical = 1.0f;
+                
+                else if (touchPos.y >= 0 && touchPos.y <= 100 && touchPos.x < maxX - 100 && touchPos.x > 100)
+                    moveVertical = -1.0f;
+                
             }
-
 
 
 		} 
