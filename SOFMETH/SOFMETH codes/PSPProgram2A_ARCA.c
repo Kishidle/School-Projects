8967cf9299125064465738be3923d33cca7aaf32
VN <arcaramon@gmail.com>
2016-07-02 21:14:48 +0800

    updated documents for 3A

--- SOFMETH/SOFMETH codes/PSPProgram2A_ARCA.c
+++ SOFMETH/SOFMETH codes/PSPProgram2A_ARCA.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 int main(){
-	FILE *fp = fopen("sofmeth 1.c", "r");
+	FILE *fp = fopen("sofmeth psp1 arca.c", "r");
 	int lineCount = 0;
+	int fLineCount = 0;
 	int multiFlag = 0;
 	int charFlag = 0;
 	int lineFlag = 0;
 	int tabFlag = 0;
 	int skipFlag = 0;
+	int openFlag = 0;
 	int functionFlag = 0;
+	int nameFlag = 0;
+	char funcName[1024] = "";
 	int i;
 	if(fp != NULL){
 		char line[1024];	
@@ -22,19 +27,73 @@ int main(){
 				// if true, then check if the line has ()
 				// also, retrieve the function name 
 				if(line[0] == 'i' && line[1] == 'n' && line[2] == 't'){
+					if(i >= 3 && line[i] != '(' && !nameFlag){
+						if(line[i + 1] == '(') nameFlag = 1;
+						if(line[i] != ' ') strncat(funcName, &line[i], 1);
+						
+					} 
+					if(line[i] == '(' || openFlag){
+						openFlag = 1;
+						if(line[i] == ')' && openFlag) functionFlag = 1;
+					} 
 					
 				}
 				else if(line[0] == 'd' && line[1] == 'o' && line[2] == 'u' && line[3] == 'b' && line[4] == 'l' && line[5] == 'e'){
+					if(i >= 6 && line[i] != '(' && !nameFlag){
+						if(line[i + 1] == '(') nameFlag = 1; 
+						if(line[i] != ' ')strncat(funcName, &line[i], 1);
+						
+					} 
+					if(line[i] == '(' || openFlag){
+						openFlag = 1;
+						if(line[i] == ')' && openFlag) functionFlag = 1;
+					}
 				}
-				else if(line[0] == 'f' && line[1] == 'l' && line[2] == 'o' && line[3] == 'a' && line[5] == 't'){
+				else if(line[0] == 'f' && line[1] == 'l' && line[2] == 'o' && line[3] == 'a' && line[4] == 't'){
+					if(i >= 5 && line[i] != '(' && !nameFlag){
+						if(line[i + 1] == '(') nameFlag = 1;
+						if(line[i] != ' ')strncat(funcName, &line[i], 1);
+						
+					}
+					
+					if(line[i] == '(' || openFlag){
+						openFlag = 1;
+						if(line[i] == ')' && openFlag) functionFlag = 1;
+					}
 					
 				}
 				else if(line[0] == 'c' && line[1] == 'h' && line[2] == 'a' && line[3] == 'r'){
-					
+					if(i >= 4 && line[i] != '(' && !nameFlag){
+						if(line[i + 1] == '(') nameFlag = 1;
+						if(line[i] != ' ')strncat(funcName, &line[i], 1);
+						
+					} 
+					if(line[i] == '(' || openFlag){
+						openFlag = 1;
+						if(line[i] == ')' && openFlag) functionFlag = 1;
+					}
 				}
 				else if(line[0] == 'v' && line[1] == 'o' && line[2] == 'i' && line[3] == 'd'){
+					if(i >= 4 && line[i] != '(' && !nameFlag){
+						if(line[i + 1] == '(') nameFlag = 1;
+						if(line[i] != ' ')strncat(funcName, &line[i], 1);
+						
+					} 
+					if(line[i] == '(' || openFlag){
+						openFlag = 1;
+						if(line[i] == ')' && openFlag) functionFlag = 1;
+					}
 				}
-				else if(line[0] == '}'){
+				if(line[0] == '}' && functionFlag){
+					functionFlag = 0;
+					nameFlag = 0;
+					openFlag = 0;
+					fLineCount++;
+					//print func name plus LOC
+					printf("%s LOC: %d\n", funcName, fLineCount);	
+					//recycle variable
+					memset(funcName, 0, sizeof(funcName));
+					fLineCount = 0;
 					//code for end of function
 				}
 				if(line[i] == '/' && line[i+1] == '*'){
@@ -64,6 +123,7 @@ int main(){
 				}
 				if(i == strlen(line) - 1 && charFlag == 1 && lineFlag == 0 && skipFlag == 0){
 					lineCount++;
+					if(functionFlag) fLineCount++;
 				}
 				skipFlag = 0;
 			}
@@ -73,6 +133,6 @@ int main(){
 			multiFlag = 0;
 		}
 		fclose(fp);
-		printf("%d", lineCount);
+		printf("Total LOC: %d", lineCount);
 	}
 }
--- SOFMETH/SOFMETH codes/sofmeth psp1 arca.c
+++ SOFMETH/SOFMETH codes/sofmeth psp1 arca.c
@@ -1,38 +1,33 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
-
+int test(){
+	printf("hello");
+}
+void fail(){
+	printf("does it work");
+	printf("yes");
+}
 int main(){
-	
 	int n;
 	int i;
 	float *arr;
 	float mean = 0;
 	float stdev = 0;
-	
 	printf("How many real numbers? ");
 	scanf("%d", &n);
-	
 	arr = malloc(n * sizeof(float));
-	
 	printf("Input %d real numbers: \n", n);
-	
 	for(i = 0; i < n; i++){
 		scanf("%f", &arr[i]);
 		mean = mean + arr[i];
-		
 	}
 	mean = mean / n;
 	printf("Mean is: %f \n", mean);
 	for(i = 0; i < n; i++){
 		stdev = stdev + pow(arr[i] - mean, 2);
-		
-		
 	}
-	
 	stdev = sqrt(stdev / (n - 1));
-	
 	printf("Standard deviation is: %f \n", stdev);
-	
-	getch();
+	getch();	
 }
