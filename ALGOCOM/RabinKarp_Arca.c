02e7f99cb778743fe07843b7d17ca88c0f0b9bb6
Noodle <arcaramon@gmail.com>
2016-07-27 00:23:51 +0800

    it bug outs on the 73rd loop for some reason

--- ALGOCOM/RabinKarp_Arca.c
+++ ALGOCOM/RabinKarp_Arca.c
@@ -9,26 +9,31 @@ int convertCI(char c){
 
 int main(int argc, char *argv[]){
 	
-	char fromText[128];
-	char input[128];
-	char pattern[128];
-	int q, i;
+	char fromText[128] = "";
+	char input[128] = "";
+	char pattern[128] = "";
+	int i, j;
+	int q = 13;
+	int patternLength = 0;
 	int flag = 0;
-	int rkArr[128] = { 0 };
+	int rkArr[64] = { 0 };
 	FILE *fpread, fpwrite;
 	int patternq = 0;
-	fpread = fopen("input.txt", "r");
-	fpwrite = fopen("output.txt", "w");
-	
+	fpread = fopen("input(4).txt", "r");
+	//fpwrite = fopen("output.txt", "w");
+	int n = 0;
+	int x =0;
+	int shift = -1;
 	if(fpread != NULL){
 		
 		while(fgets(fromText, sizeof(fromText), fpread) != NULL){
 			
 			int length = strlen(fromText);
-			
+			printf("d\n\n%d", x);
 			if(fromText[length - 1] == '\n') 
-				fromText[length - 1] = '\0'
+				fromText[length - 1] = '\0';
 			
+			printf("%s\n", fromText);
 			//get text and pattern
 			
 			for(i = 0; i < length; i++){
@@ -36,23 +41,104 @@ int main(int argc, char *argv[]){
 				if(fromText[i] != '.'){
 					
 					if(flag){
-						patternq = patternq + convertCI(fromText[i]);
+						
+						//patternLength++; //decide whether to use this one or put it in an array instead
+						strncat(pattern, &fromText[i], 1);
+						patternq = patternq + (int)fromText[i];
+						
 					}
 					
-					else rkArr[i] = convertCI(fromText[i]);
+					else{
+						strncat(input, &fromText[i], 1);
+						//kArr[i] = convertCI(fromText[i]);
+						
+						
+					}
+					
+				
+					 
 				}
 				
 				
 				else if(fromText[i] == '.')
 					flag = 1;
 				
+			}
+			
+			patternq = patternq % q;
+			flag = 0;
+			// do we just add the ascii values or are we supposed to concatenate the integers together?
+			for(i = 0; i < strlen(input); i++){
 				
+				if(strlen(input) - i < strlen(pattern)){
+					//printf("\n%d", i);
+					break;
+				}
+					
+				for(j = 0; j < strlen(pattern); j++){
+					
+					rkArr[i] = rkArr[i] + (int)input[i + j];
+					//printf("%d %d\n", j, (int)input[i+j]);
+				}
+				//printf("\n");
+				rkArr[i] = rkArr[i] % q;
+				
+				//printf("mod: %d \n", rkArr[i]);
+				//printf("pattern: %d \n", patternq);
+				++n;
 				
 			}
-			//convert text into int
 			
+			for(i = 0; i < n; ++i){
+				
+				if(rkArr[i] == patternq){
+					
+					
+					for(j = 0; j < strlen(pattern); ++j){
+						
+						printf("%d", i);
+						
+						if(input[i + j] == pattern[j]){
+						
+							flag = 1;
+						}
+						else{
+							flag = 0;
+							if(x == 73){
+							printf("helloagain");
+							}
+							break;
+						}
+						
+					}
+					if(x == 73){
+						printf("testerino");
+					}
+					//check the string?
+				}
 			
+				if(flag){
+					if(x == 73){
+							printf("flagerino");
+						}
+					flag = 0;
+					printf("Pattern occurs with shift %d\n\n", i);
+					shift = 1;
+				}
+				
+			}
+			printf("test");
+			if(shift == -1){
+				printf("Invalid shift\n\n");
+			}
 			
+			memset(input, 0, sizeof(input));
+			memset(pattern, 0, sizeof(pattern));
+			flag = 0;
+			shift = -1;
+			patternq = 0;
+			memset(rkArr, 0, sizeof(rkArr));
+			x++;
 		}
 	}
 	
